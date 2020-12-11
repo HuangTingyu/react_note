eefc1a7f6b6967208b886f107f02d177b8851aca
sakurahuang <sakurahuang@tencent.com>
2020-12-11 21:10:01 +0800

    Antd 初步

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import TodoList from './TodoList';
-import App from './App'
+import TodoList from './AntdTodoList/TodoList';
+// import App from './App'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <TodoList />
   </React.StrictMode>,
   document.getElementById('root')
 );
